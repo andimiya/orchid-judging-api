879eaa42d6210bb275761a548f3522dbc2bc6a13
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-12-17 15:02:04 -1000

    Added the password column

--- createTables.sql
+++ createTables.sql
@@ -1,27 +1,9 @@
 START TRANSACTION;
 
-CREATE TABLE Users (
-  id serial primary key,
-  first_name text default null,
-  last_name text default null,
-  email text not null
-);
+CREATE TABLE Users ( id serial primary key, first_name text default null, last_name text default null, email text not null, password text not null);
 
-CREATE TABLE Crypto_Types (
-  id serial primary key,
-  symbol text not null,
-  name text not null
-);
+CREATE TABLE Crypto_Types (id serial primary key, symbol text not null, name text not null);
 
-CREATE TABLE Transactions (
-  id serial primary key,
-  crypto_type_id integer references Crypto_Types(id),
-  user_id integer references Users(id),
-  usd_invested float8 not null,
-  coin_purchased float8 not null,
-  exchange_rate float8 not null,
-  created_at timestamp,
-  updated_at timestamp
-);
+CREATE TABLE Transactions (id serial primary key, crypto_type_id integer references Crypto_Types(id), user_id integer references Users(id), usd_invested float8 not null, coin_purchased float8 not null, exchange_rate float8 not null, created_at timestamp, updated_at timestamp);
 
 COMMIT;
