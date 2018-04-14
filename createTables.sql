21f1f8838befbf52586c7652cd3ab74e0c8210e4
Andrea Takamiya <takamiya.andrea@gmail.com>
2018-04-14 10:20:02 -1000

    Updated seeds

--- createTables.sql
+++ createTables.sql
@@ -7,22 +7,4 @@ CREATE TABLE Users (
   email text not null UNIQUE
 );
 
-CREATE TABLE Crypto_Types (
-  id serial primary key,
-  symbol text not null,
-  name text not null
-);
-
-CREATE TABLE Transactions (
-  id serial primary key,
-  crypto_type_id integer references Crypto_Types(id),
-  user_id integer references Users(id),
-  usd_invested float8 not null,
-  coin_purchased float8 not null,
-  exchange_rate float8 not null,
-  created_at timestamp,
-  updated_at timestamp,
-  purchased_at timestamp
-);
-
 COMMIT;
